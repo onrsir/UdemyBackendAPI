cbdfff047deb97bccec1d4ae88fc3e016d0d38df
onur sir <oonursir@gmail.com>
2024-03-04 23:24:29 +0300

    Initial commit

---
+++ src/main/java/com/onursir/UdemyBackendAPI/UdemyBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.onursir.UdemyBackendAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyBackendApiApplication.class, args);
+	}
+
+}
